5b30723a1f92792c520d0ef7bdc33f5c10c08b6b
Young <jd95288@163.com>
2024-03-21 08:55:50 +0800

    区域和检索，取消查询时的条件判断

--- src/main/java/easy/NumArray.java
+++ src/main/java/easy/NumArray.java
@@ -18,20 +18,14 @@ public class NumArray {
     private final int[] prefixSum;
 
     public NumArray(int[] nums) {
-        prefixSum = new int[nums.length];
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        prefixSum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
         }
     }
 
     public int sumRange(int left, int right) {
-        // [left, right]，注意下标为0的情况
-        if (left > 0) {
-            return prefixSum[right] - prefixSum[left - 1];
-        } else {
-            return prefixSum[right];
-        }
+        return prefixSum[right + 1] - prefixSum[left];
     }
 
     public static void main(String[] args) {
