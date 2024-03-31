63539489ae9c5c72cf6bcc7a76781248bf107328
Young <jd95288@163.com>
2024-03-31 22:51:23 +0800

    两数之和，执行通过

---
+++ src/main/java/easy/TwoSum1.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，
+ * 请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ *
+ * @author jd95288
+ * @date 2024-03-31 21:17
+ */
+public class TwoSum1 {
+
+    public int[] twoSum_v1(int[] nums, int target) {
+        Map<Integer, Integer> hashtable = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
