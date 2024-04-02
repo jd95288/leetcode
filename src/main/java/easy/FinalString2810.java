0ee94b7b97469c846681a658adef3637a3c7e0df
Young <jd95288@163.com>
2024-04-02 23:24:31 +0800

    故障键盘，执行通过

---
+++ src/main/java/easy/FinalString2810.java
@@ -0,0 +1,25 @@
+package easy;
+
+/**
+ * 故障键盘
+ * 你的笔记本键盘存在故障，每当你在上面输入字符 'i' 时，它会反转你所写的字符串。而输入其他字符则可以正常工作。
+ * 给你一个下标从 0 开始的字符串 s ，请你用故障键盘依次输入每个字符。
+ * 返回最终笔记本屏幕上输出的字符串。
+ *
+ * @author jd95288
+ * @date 2024-04-01 8:42
+ */
+public class FinalString2810 {
+    public String finalString(String s) {
+        StringBuilder res = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if ('i' == chars[i]) {
+                res.reverse();
+            } else {
+                res.append(chars[i]);
+            }
+        }
+        return res.toString();
+    }
+}
