3d474d0e2f3192ab46d322a64b5833d7f42d3af0
Young <jd95288@163.com>
2024-04-11 23:08:27 +0800

    移动零，执行通过

---
+++ src/main/java/easy/MoveZeroes283.java
@@ -0,0 +1,28 @@
+package easy;
+
+/**
+ * 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 请注意 ，必须在不复制数组的情况下原地对数组进行操作。
+ *
+ * @author jd95288
+ * @date 2024-04-10 21:51
+ */
+public class MoveZeroes283 {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes283 main = new MoveZeroes283();
+        main.moveZeroes(new int[]{0, 1, 0, 3, 12});
+    }
+}
