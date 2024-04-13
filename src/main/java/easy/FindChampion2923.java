c218551425a5d3b81438079e6fd184e83ccfacc7
Young <jd95288@163.com>
2024-04-13 20:10:06 +0800

    找到冠军 I，执行通过

---
+++ src/main/java/easy/FindChampion2923.java
@@ -0,0 +1,31 @@
+package easy;
+
+import java.util.Arrays;
+
+/**
+ * 找到冠军 I
+ * 一场比赛中共有 n 支队伍，按从 0 到  n - 1 编号。
+ * 给你一个下标从 0 开始、大小为 n * n 的二维布尔矩阵 grid 。对于满足 0 <= i, j <= n - 1 且 i != j 的所有 i, j ：如果 grid[i][j] == 1，那么 i 队比 j 队 强 ；否则，j 队比 i 队 强 。
+ * 在这场比赛中，如果不存在某支强于 a 队的队伍，则认为 a 队将会是 冠军 。
+ * 返回这场比赛中将会成为冠军的队伍。
+ *
+ * @author jd95288
+ * @date 2024-04-12 0:13
+ */
+public class FindChampion2923 {
+
+    public int findChampion(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+            }
+            if (sum == n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
