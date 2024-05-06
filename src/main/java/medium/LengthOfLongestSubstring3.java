bc86864e71051ae3171a2fa3b7b92f8748c37f05
Young <jd95288@163.com>
2024-05-06 23:06:26 +0800

    无重复字符的最长子串，执行通过

---
+++ src/main/java/medium/LengthOfLongestSubstring3.java
@@ -0,0 +1,36 @@
+package medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 无重复字符的最长子串
+ * <p>
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串的长度。
+ *
+ * @author jd95288
+ * @date 2024-05-05 20:35
+ */
+public class LengthOfLongestSubstring3 {
+    public int lengthOfLongestSubstring(String s) {
+        char[] c = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        int res = 0;
+        int l = 0;
+        for (int i = 0; i < c.length; i++) {
+            if (map.get(c[i]) == null) {
+                map.put(c[i], i);
+                l++;
+                res = Math.max(res, l);
+            } else {
+                res = Math.max(res, l);
+                l = i - map.get(c[i]);
+                map.clear();
+                for (int j = i - l + 1; j <= i; j++) {
+                    map.put(c[j], j);
+                }
+            }
+        }
+        return res;
+    }
+}
