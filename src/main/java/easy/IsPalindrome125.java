b74b85dc4501625c613d9d541a326fcd803867ac
Young <jd95288@163.com>
2024-06-09 22:29:38 +0800

    验证回文串，执行通过

---
+++ src/main/java/easy/IsPalindrome125.java
@@ -0,0 +1,31 @@
+package easy;
+
+
+/**
+ * 验证回文串
+ * <p>
+ * 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。
+ * 则可以认为该短语是一个 回文串 。
+ * 字母和数字都属于字母数字字符。
+ * 给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+ *
+ * @author jd95288
+ * @date 2024-06-09 21:45
+ */
+public class IsPalindrome125 {
+
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                c = (char) ((int) c + 32);
+            }
+            if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
+                sb.append(c);
+            }
+        }
+        return sb.toString().equals(sb.reverse().toString());
+    }
+
+}
