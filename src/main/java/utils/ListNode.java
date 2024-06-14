73ad5010c733d53aaae3d1468cfb2c28625ead09
Young <jd95288@163.com>
2024-06-14 22:46:07 +0800

    公共类定义

---
+++ src/main/java/utils/ListNode.java
@@ -0,0 +1,23 @@
+package utils;
+
+
+/**
+ * @author jd95288
+ * @date 2024-05-19 17:26
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
