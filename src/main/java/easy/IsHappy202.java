b42249dc72b0a2c5a09e373bf08d3b520d7f8336
Young <jd95288@163.com>
2024-06-15 23:33:36 +0800

    快乐数，执行通过

---
+++ src/main/java/easy/IsHappy202.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 快乐数
+ * <p>
+ * 编写一个算法来判断一个数 n 是不是快乐数。
+ * 「快乐数」 定义为：
+ * 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ * 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ * 如果这个过程 结果为 1，那么这个数就是快乐数。
+ * 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+ *
+ * @author jd95288
+ * @date 2024-06-15 20:59
+ */
+public class IsHappy202 {
+
+    private int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+}
