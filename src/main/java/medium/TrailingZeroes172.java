cb52c941b301fb583d84cf6fdddc47b421b57674
Young <jd95288@163.com>
2024-08-10 23:57:14 +0800

    阶乘后的零，执行通过

---
+++ src/main/java/medium/TrailingZeroes172.java
@@ -0,0 +1,22 @@
+package medium;
+
+/**
+ * 阶乘后的零
+ *
+ * 给定一个整数 n ，返回 n! 结果中尾随零的数量。
+ * 提示 n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1
+ *
+ * @author jd95288
+ * @date 2024-08-10 23:19
+ */
+public class TrailingZeroes172 {
+    public int trailingZeroes(int n) {
+        int ans = 0;
+        for (int i = 5; i <= n; i += 5) {
+            for (int x = i; x % 5 == 0; x /= 5) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
