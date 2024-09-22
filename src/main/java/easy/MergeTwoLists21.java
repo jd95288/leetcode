22d663920a013fbba9a35cdacf7ff07a0ce00445
Young <jd95288@163.com>
2024-09-22 15:30:34 +0800

    合并两个有序链表，执行通过

---
+++ src/main/java/easy/MergeTwoLists21.java
@@ -0,0 +1,32 @@
+package easy;
+
+import utils.ListNode;
+
+/**
+ * 合并两个有序链表
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ *
+ * @author jd95288
+ * @date 2024-09-21 22:12
+ */
+public class MergeTwoLists21 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
+}
