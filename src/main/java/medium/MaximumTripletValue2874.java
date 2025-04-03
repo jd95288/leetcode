5a696f009c06bf251ef0f3f1a63ef707e62bcd97
Young <jd95288@163.com>
2025-04-03 20:37:56 +0800

    有序三元组中的最大值 II，执行通过

---
+++ src/main/java/medium/MaximumTripletValue2874.java
@@ -0,0 +1,27 @@
+package medium;
+
+/**
+ * 有序三元组中的最大值 II
+ *
+ * 给你一个下标从 0 开始的整数数组 nums 。
+ * 请你从所有满足 i < j < k 的下标三元组 (i, j, k) 中，找出并返回下标三元组的最大值。如果所有满足条件的三元组的值都是负数，则返回 0 。
+ * 下标三元组 (i, j, k) 的值等于 (nums[i] - nums[j]) * nums[k] 。
+ *
+ * @author jd95288
+ * @date 2025-04-03 0:41
+ */
+public class MaximumTripletValue2874 {
+
+    public long maximumTripletValue(int[] nums) {
+        long res = 0L;
+        int n = nums.length;
+        int max = 0;
+        long maxDiff = 0;
+        for (int i = 0; i < n - 1; i++) {
+            max = Math.max(max, nums[i]);
+            maxDiff = Math.max(maxDiff, max - nums[i]);
+            res = Math.max(res, maxDiff * nums[i + 1]);
+        }
+        return res;
+    }
+}
