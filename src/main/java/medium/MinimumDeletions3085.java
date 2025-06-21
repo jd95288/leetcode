4e5362ec9145bf11b3d527488389341dfcf92c19
Young <jd95288@163.com>
2025-06-21 22:21:07 +0800

    成为 K 特殊字符串需要删除的最少字符数，todo

---
+++ src/main/java/medium/MinimumDeletions3085.java
@@ -0,0 +1,17 @@
+package medium;
+
+
+/**
+ * 成为 K 特殊字符串需要删除的最少字符数
+ * <p>
+ * 给你一个字符串 word 和一个整数 k。
+ * 如果 |freq(word[i]) - freq(word[j])| <= k 对于字符串中所有下标 i 和 j  都成立，则认为 word 是 k 特殊字符串。
+ * 此处，freq(x) 表示字符 x 在 word 中的出现频率，而 |y| 表示 y 的绝对值。
+ * 返回使 word 成为 k 特殊字符串 需要删除的字符的最小数量。
+ *
+ * @author jd95288
+ * @date 2025-06-21 20:58
+ */
+public class MinimumDeletions3085 {
+
+}
