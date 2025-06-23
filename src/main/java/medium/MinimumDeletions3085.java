971cd6134a7d6c6eb9e0f56351188d4040472f8d
Young <jd95288@163.com>
2025-06-23 22:14:05 +0800

    成为 K 特殊字符串需要删除的最少字符数，执行通过

--- src/main/java/medium/MinimumDeletions3085.java
+++ src/main/java/medium/MinimumDeletions3085.java
@@ -1,5 +1,6 @@
 package medium;
 
+import java.util.Arrays;
 
 /**
  * 成为 K 特殊字符串需要删除的最少字符数
@@ -13,5 +14,29 @@ package medium;
  * @date 2025-06-21 20:58
  */
 public class MinimumDeletions3085 {
+
+    public int minimumDeletions(String word, int k) {
+        int[] cnt = new int[26];
+        char[] chars = word.toCharArray();
+        for (char c : chars) {
+            cnt[c - 'a']++;
+        }
+        Arrays.sort(cnt);
+        int maxLength = 0;
+        int frequency = 0;
+        int r = 0;
+        for (int l = 0; l < 26; l++) {
+            if (cnt[l] == 0) {
+                continue;
+            }
+            while (r < 26 && cnt[l] + k >= cnt[r]) {
+                frequency += cnt[r];
+                r++;
+            }
+            maxLength = Math.max(maxLength, frequency + (26 - r) * (cnt[l] + k));
+            frequency -= cnt[l];
+        }
+        return word.length() - maxLength;
+    }
 
 }
