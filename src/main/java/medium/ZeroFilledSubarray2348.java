db1a3a4f72a47a46971b017d79e7af07a997a8d4
Young <jd95288@163.com>
2025-08-16 21:00:10 +0800

    全 0 子数组的数目，执行通过

--- src/main/java/medium/ZeroFilledSubarray2348.java
+++ src/main/java/medium/ZeroFilledSubarray2348.java
@@ -2,7 +2,7 @@ package medium;
 
 /**
  * 全 0 子数组的数目
- *
+ * <p>
  * 给你一个整数数组 nums ，返回全部为 0 的 子数组 数目。
  * 子数组 是一个数组中一段连续非空元素组成的序列。
  *
@@ -15,13 +15,13 @@ public class ZeroFilledSubarray2348 {
         long res = 0L;
         int n = nums.length;
         int i = 0;
-        while (i < n){
-            if (nums[i] != 0){
+        while (i < n) {
+            if (nums[i] != 0) {
                 i++;
                 continue;
             }
             int start = i;
-            while (i < n && nums[i] == 0){
+            while (i < n && nums[i] == 0) {
                 i++;
             }
             int cnt = i - start;
