af1907d45a9ca26305795ea5af5dc69b83f9764c
Young <jd95288@163.com>
2025-08-27 20:52:22 +0800

    对角线最长的矩形的面积，执行通过

--- src/main/java/easy/AreaOfMaxDiagonal3000.java
+++ src/main/java/easy/AreaOfMaxDiagonal3000.java
@@ -2,7 +2,7 @@ package easy;
 
 /**
  * 对角线最长的矩形的面积
- * <p>
+ *
  * 给你一个下标从 0 开始的二维整数数组 dimensions。
  * 对于所有下标 i（0 <= i < dimensions.length），dimensions[i][0] 表示矩形 i 的长度，而 dimensions[i][1] 表示矩形 i 的宽度。
  * 返回对角线最 长 的矩形的 面积 。如果存在多个对角线长度相同的矩形，返回面积最 大 的矩形的面积。
