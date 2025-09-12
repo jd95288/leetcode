7787c3392fea8e056033c172ab6be4b63f0a991d
Young <jd95288@163.com>
2025-09-12 23:43:55 +0800

    字符串元音游戏，执行通过

---
+++ src/main/java/medium/DoesAliceWin3227.java
@@ -0,0 +1,29 @@
+package medium;
+
+/**
+ * 字符串元音游戏
+ * <p>
+ * 小红和小明在玩一个字符串元音游戏。
+ * 给你一个字符串 s，小红和小明将轮流参与游戏，小红 先 开始：
+ * 在小红的回合，她必须移除 s 中包含 奇数 个元音的任意 非空 子字符串。
+ * 在小明的回合，他必须移除 s 中包含 偶数 个元音的任意 非空 子字符串。
+ * <p>
+ * 第一个无法在其回合内进行移除操作的玩家输掉游戏。假设小红和小明都采取 最优策略 。
+ * 如果小红赢得游戏，返回 true，否则返回 false。
+ * 英文元音字母包括：a, e, i, o, 和 u。
+ *
+ * @author jd95288
+ * @date 2025-09-12 8:43
+ */
+public class DoesAliceWin3227 {
+
+    public boolean doesAliceWin(String s) {
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
